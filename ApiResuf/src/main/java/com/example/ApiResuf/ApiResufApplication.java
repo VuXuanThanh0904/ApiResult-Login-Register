ad461f8e6f258d7a3068bb689b465cb249764680
VuXuanThanh0904 <vxtdev@gmail.com>
2022-12-03 02:34:05 +0700

    abc

---
+++ ApiResuf/src/main/java/com/example/ApiResuf/ApiResufApplication.java
@@ -0,0 +1,13 @@
+package com.example.ApiResuf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiResufApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiResufApplication.class, args);
+	}
+
+}
